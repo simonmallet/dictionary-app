683cdda58fe034db1fc3f551aee13695a5d09b18
Simon Mallet <simonmallet@Simons-MacBook-Pro.local>
2017-03-13 16:54:44 +1300

    ajax loader, formatted display

--- themes/simple/javascript/script.js
+++ themes/simple/javascript/script.js
@@ -84,39 +84,84 @@
 			});
 		}
 
+		var funnyTitles = [
+			'You talking to me?',
+			'I never believed it existed!',
+			'Clever... Very clever!',
+			'Go ahead, make my day.',
+			'Show me the money!'
+		];
+
+        $('#Form_QueryDictionary_Word').focus();
+
 		// Ajax call for query dictionary
 		$('#Form_QueryDictionary_action_doHandleDictionary').click(function (e) {
 			e.preventDefault();
+			var searchedWord = $('#Form_QueryDictionary_Word').val();
 
-            var url = "http://dictionary-service.simonmallet.com/public/infos/" + $('#Form_QueryDictionary_Word').val();
+			// @todo: To be changed to a config.
+            var url = "http://dictionary-service.simonmallet.com/public/infos/" + searchedWord;
 
             // resetting results
 			$('#word-result').hide();
+            $('#word-results-stats').hide();
             $('#word-results-words').html('');
+            $('#word-results-title-word').html(searchedWord);
+            $('#word-results-title-funny').html(funnyTitles[getRandomInt(0,funnyTitles.length-1)]);
+
+            // Display loading image
+            $('.loading-word-results').show();
 
             $.ajax({
             	type: "GET",
 				url: url
-            	})
-                .done(function (response) {
-                    $('#word-result').show();
-                	console.log('success');
-                	console.log(response);
+			})
+			.done(function (response) {
+				$('#word-result').show();
 
-                	if (response[0] == "No record found.") {
-                        $('#word-results-words').html('Whoops! Looks like that word does not exist in this dictionary.');
-                		return;
+				// @todo: The service should return a 404 instead of a 200.
+				if (response[0] == "No record found.") {
+					$('#word-results-words').html('Whoops! Looks like that word does not exist in this dictionary.');
+					return;
+				}
+
+				$('#word-results-stats').show();
+				$('#stats-word-in-other-definitions').html('');
+
+				response.wordDefinitions.forEach(function(word) {
+					$('#word-results-words').append('<div class=word-type>' + word.wordtype + '</div><div class="word-definition">' + word.definition + '</div>');
+				});
+				var presenceOfLetter = response.statistics.totalWordsInFirstLetter / response.statistics.totalWordsInDictionary * 100;
+				$('#stats-percent-letter').html('The letter "'+ searchedWord.charAt(0).toUpperCase() +'" accounts for ' + presenceOfLetter.toPrecision(3) + '% of all words in the dictionary!');
+
+				if (!!response.statistics.wordUsedInOtherDefinitions && response.statistics.wordUsedInOtherDefinitions.length > 0) {
+                    $('#stats-word-in-other-definitions').append('The following words mention the word ' + searchedWord + ': ');
+                    for (var i = 0; i < response.statistics.wordUsedInOtherDefinitions.length; i++) {
+                    	if (i > 5) {
+                    		break;
+						}
+                        $('#stats-word-in-other-definitions').append(response.statistics.wordUsedInOtherDefinitions[i].word + ', ');
 					}
+				}
 
-					response.wordDefinitions.forEach(function(word) {
-						$('#word-results-words').append('<div class=word-type>' + word.wordtype + '</div><div class="word-definition">' + word.definition + '</div>');
-					});
-					console.log(response.statistics);
-                })
-                .fail(function (xhr) {
-                	console.log('failure');
-					console.log(xhr);
-                });
+                console.log(response.statistics);
+			})
+			.fail(function (xhr) {
+				console.log('failure');
+				console.log(xhr);
+			})
+			.always(function () {
+                // Display loading image
+                $('.loading-word-results').hide();
+			});
 		});
+
+        /**
+         * Returns a random integer between min (inclusive) and max (inclusive)
+         * Using Math.round() will give you a non-uniform distribution!
+         */
+        function getRandomInt(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
 	});
 }(jQuery));
--- themes/simple/templates/Layout/Page.ss
+++ themes/simple/templates/Layout/Page.ss
@@ -1,10 +1,21 @@
 <% include SideBar %>
 <div class="content-container unit size3of4 lastUnit">
 	<article>
+        <div class="loading-word-results" id="loading-word-results">
+            <div class="loading-title" id="loading-title">
+                Loading...
+            </div>
+            <img src="../mysite/images/ajax-loader.gif">
+        </div>
         <div id="word-result" class="word-result">
-            <div class="title">What did we dig up here...</div>
+            <div class="stats" id="word-results-stats">
+                <div id="stats-title">Statistics</div>
+                <div class="stats-words" id="stats-percent-letter"></div>
+                <div class="stats-words" id="stats-word-in-other-definitions"></div>
+            </div>
+            <div class="title" id="word-results-title-word">Word here</div>
+            <div class="title" id="word-results-title-funny">title here</div>
             <div class="words" id="word-results-words"></div>
-            <div class="stats" id="word-results-stats"></div>
         </div>
 		<h1>$Title</h1>
         <% if $Title == "Home" %>
